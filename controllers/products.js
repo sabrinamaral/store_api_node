63a97187f36b10dfc69a10d6cd6300dc26f56535
sabrinamaral <sabrinamaral@hotmail.com>
2023-03-28 21:34:35 -0300

    controler for get all products

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,12 @@
+const Products = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  res.status(200).json({});
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const products = await Products.find(req.query);
+
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
