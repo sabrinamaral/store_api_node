fdc28f81f3ccde3cbde427381e8eadb7aa6468dd
sabrinamaral <sabrinamaral@hotmail.com>
2023-04-01 21:39:32 -0300

    filter, sort and select

--- controllers/products.js
+++ controllers/products.js
@@ -4,8 +4,36 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({});
 };
 const getAllProducts = async (req, res) => {
-  const products = await Products.find(req.query);
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObj = {};
 
+  // filtering by featured, name or/and company
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+  let result = Products.find(queryObj);
+
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("name");
+  }
+
+  //selecting fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
